6c9fa31ef4e386007e4dfad6b806764d2b37f029
Jochem Hagenaars <jochem.hagenaars@gmail.com>
2023-06-16 10:01:07 +0200

    WIP

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,10 @@ import { fileURLToPath, URL } from 'node:url';
 
 import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
+import path from 'path';
 import { defineConfig } from 'vite';
+
+const pathSrc = path.resolve(__dirname, './src');
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,6 +16,12 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+
+  css: {
+    preprocessorOptions: {
+      scss: { additionalData: `@import "${pathSrc}/assets/scss/app.scss";` },
     },
   },
 });
